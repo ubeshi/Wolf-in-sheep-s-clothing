aa4234aa34604d20d25db70bd9978b87330348c0
Colin Tong <tongcolin@live.ca>
2022-11-05 21:54:18 -0400

    Added item selection and dummy items

---
+++ src/items/empty-item/empty-item.gd
@@ -0,0 +1,9 @@
+extends PlayerItem
+
+class_name EmptyItem
+
+func _init():
+    self.icon_image_large = "res://assets/items/empty-item/empty-item-large.png";
+    self.icon_image_small = "res://assets/items/empty-item/empty-item-small.png";
+    self.label = "You have no items";
+    self.description = "";
---
+++ src/items/item.gd
@@ -0,0 +1,8 @@
+extends Node
+
+class_name PlayerItem
+
+var icon_image_small: String;
+var icon_image_large: String;
+var label: String;
+var description: String;
