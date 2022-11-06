92b585159b16ab5ab472ac7945c2df8cbc7c8e3b
Colin Tong <tongcolin@live.ca>
2022-11-06 01:40:49 -0400

    Try loading debug cube scene instead

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -3,12 +3,6 @@ extends Spatial
 var exploration_ui = preload("res://scenes/user-interface/item-menu.tscn");
 var ui = preload("res://scenes/ui.tscn");
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     # Add exploration ui
     var exploration_ui_node = exploration_ui.instance();
