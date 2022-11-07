5725184dccc24dcca6035a23509bdfdfb9574eb7
anson-li <ansonli.94@gmail.com>
2022-11-07 13:49:08 -0700

    swapped to all surfaces

--- src/scenes/characters/wolf/Wolf.gd
+++ src/scenes/characters/wolf/Wolf.gd
@@ -1,14 +1,18 @@
 extends StaticBody
 
+onready var model = get_node("WolfModel/aleu001");
+
 func hover_focus() -> void:
-    var material = get_node("WolfModel/aleu001").get_active_material(0).duplicate();
-    material.albedo_color = Color(255, 0, 0, 1);
-    get_node("WolfModel/aleu001").set_surface_material(0, material);
+    for index in model.get_surface_material_count():
+        var material = model.get_active_material(index).duplicate();
+        material.albedo_color = Color(255, 0, 0, 1);
+        model.set_surface_material(index, material);
 
 func hover_unfocus() -> void:
-    var material = get_node("WolfModel/aleu001").get_active_material(0).duplicate();
-    material.albedo_color = Color(0, 0, 0, 1);
-    get_node("WolfModel/aleu001").set_surface_material(0, material);
+    for index in model.get_surface_material_count():
+        var material = model.get_active_material(index).duplicate();
+        material.albedo_color = Color(0, 0, 0, 1);
+        model.set_surface_material(index, material);
 
 func interact() -> void:
     # TODO: Add in dialogic here
