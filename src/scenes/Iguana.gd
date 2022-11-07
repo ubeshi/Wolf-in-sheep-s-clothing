f7d4106079b025577b09043eda8d5cc806097a64
anson-li <ansonli.94@gmail.com>
2022-11-07 15:22:03 -0700

    Refactored hover functions and extended test to iguana

--- src/scenes/Iguana.gd
+++ src/scenes/Iguana.gd
@@ -1,14 +1,15 @@
 extends StaticBody
 
+onready var model = get_node("IguanaModel/10668_Iguana_v2");
+onready var original_material = get_node("IguanaModel/10668_Iguana_v2").get_surface_material(0).duplicate();
+
 func hover_focus() -> void:
-    var material = get_node("IguanaModel/10668_Iguana_v2").get_active_material(0).duplicate();
-    material.albedo_color = Color(255, 0, 0, 255);
-    get_node("IguanaModel/10668_Iguana_v2").set_surface_material(0, material);
+    var material = original_material.duplicate();
+    material.albedo_color = Color(255, 0, 0, 0.6);
+    model.set_surface_material(0, material);
 
 func hover_unfocus() -> void:
-    var material = get_node("IguanaModel/10668_Iguana_v2").get_active_material(0).duplicate();
-    material.albedo_color = Color(255, 53, 53, 255);
-    get_node("IguanaModel/10668_Iguana_v2").set_surface_material(0, material);
+    model.set_surface_material(0, original_material);
 
 func interact() -> void:
     # TODO: Add in dialogic here
--- src/scenes/characters/wolf/Wolf.gd
+++ src/scenes/characters/wolf/Wolf.gd
@@ -1,14 +1,15 @@
 extends StaticBody
 
+onready var model = get_node("WolfModel/aleu001");
 onready var original_material = get_node("WolfModel/aleu001").get_surface_material(0).duplicate();
 
 func hover_focus() -> void:
-    var material = get_node("WolfModel/aleu001").get_surface_material(0).duplicate();
+    var material = original_material.duplicate();
     material.albedo_color = Color(255, 0, 0, 0.6);
-    get_node("WolfModel/aleu001").set_surface_material(0, material);
+    model.set_surface_material(0, material);
 
 func hover_unfocus() -> void:
-    get_node("WolfModel/aleu001").set_surface_material(0, original_material);
+    model.set_surface_material(0, original_material);
 
 func interact() -> void:
     # TODO: Add in dialogic here
