5da3674a88493f42993923d1a66b31e8f21cf380
anson-li <ansonli.94@gmail.com>
2022-11-07 16:12:24 -0700

    Separated out onclick triggers for iguana and wolf

--- src/scenes/characters/wolf/wolf.gd
+++ src/scenes/characters/wolf/wolf.gd
@@ -4,3 +4,5 @@ func _ready():
     model = get_node("WolfModel/aleu001");
     original_material = get_node("WolfModel/aleu001").get_surface_material(0).duplicate();
 
+func interact() -> void:
+    print('afoow');
