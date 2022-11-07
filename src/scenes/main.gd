c02970defac37cced0e933a95739cfe1ea5b9a44
Colin Tong <tongcolin@live.ca>
2022-11-06 23:15:37 -0500

    Render with no navigation

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -3,8 +3,8 @@ extends Spatial
 func _ready() -> void:
     # Add item menu
     add_scene("res://scenes/user-interface/item-menu.tscn");
-    # Add debug2 scene
-    add_scene("res://scenes/debug2.tscn");
+    # Add ui scene
+    add_scene("res://scenes/ui.tscn");
 
 func add_scene(path: String) -> void:
     var scene = load(path);
