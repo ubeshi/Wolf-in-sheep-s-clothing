8cbf41584a47da6c065321e7c0ec617a2d182f7f
Colin Tong <tongcolin@live.ca>
2022-11-06 23:00:13 -0500

    Switch back to debug scene

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -3,8 +3,8 @@ extends Spatial
 func _ready() -> void:
     # Add item menu
     add_scene("res://scenes/user-interface/item-menu.tscn");
-    # Add foyer scene
-    add_scene("res://scenes/foyer.tscn");
+    # Add debug scene
+    add_scene("res://scenes/debug.tscn");
 
 func add_scene(path: String) -> void:
     var scene = load(path);
