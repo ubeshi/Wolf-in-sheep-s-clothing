f22464625d2101f2dabe61474f362efeb4bb0b88
Colin Tong <tongcolin@live.ca>
2022-11-06 20:55:16 -0500

    Return to debug scene render

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,12 +1,15 @@
 extends Spatial
 
 var exploration_ui = load("res://scenes/user-interface/item-menu.tscn");
-var ui = load("res://scenes/ui.tscn");
+# var ui = load("res://scenes/ui.tscn");
 
 func _ready() -> void:
-    # Add exploration ui
-    var exploration_ui_node = exploration_ui.instance();
-    add_child(exploration_ui_node);
-    # Enter ui scene
-    var ui_node = ui.instance();
-    add_child(ui_node);
+    # Add item menu
+    add_scene("res://scenes/user-interface/item-menu.tscn");
+    # Add debug scene
+    add_scene("res://scenes/debug.tscn");
+
+func add_scene(path: String) -> void:
+    var scene = load(path);
+    var node = scene.instance();
+    add_child(node);
