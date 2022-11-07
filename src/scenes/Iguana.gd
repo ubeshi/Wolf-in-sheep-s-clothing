c0773120b625ce9583dd8556f7bf590a9bedaed4
anson-li <ansonli.94@gmail.com>
2022-11-07 14:45:16 -0700

    Change alpha to be much higher

--- src/scenes/Iguana.gd
+++ src/scenes/Iguana.gd
@@ -2,12 +2,12 @@ extends StaticBody
 
 func hover_focus() -> void:
     var material = get_node("IguanaModel/10668_Iguana_v2").get_active_material(0).duplicate();
-    material.albedo_color = Color(255, 0, 0, 1);
+    material.albedo_color = Color(255, 0, 0, 255);
     get_node("IguanaModel/10668_Iguana_v2").set_surface_material(0, material);
 
 func hover_unfocus() -> void:
     var material = get_node("IguanaModel/10668_Iguana_v2").get_active_material(0).duplicate();
-    material.albedo_color = Color(0, 0, 0, 1);
+    material.albedo_color = Color(255, 53, 53, 255);
     get_node("IguanaModel/10668_Iguana_v2").set_surface_material(0, material);
 
 func interact() -> void:
