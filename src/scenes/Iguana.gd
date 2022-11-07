d7499462b4681dcc2a764ea0a6cce2af46f60c25
anson-li <ansonli.94@gmail.com>
2022-11-07 14:29:03 -0700

    Revert "swapped to all surfaces"
    This reverts commit 5725184dccc24dcca6035a23509bdfdfb9574eb7.

--- src/scenes/Iguana.gd
+++ src/scenes/Iguana.gd
@@ -1,18 +1,14 @@
 extends StaticBody
 
-onready var model = get_node("IguanaModel/10668_Iguana_v2");
-
 func hover_focus() -> void:
-    for index in model.get_surface_material_count():
-        var material = model.get_active_material(index).duplicate();
-        material.albedo_color = Color(255, 0, 0, 1);
-        model.set_surface_material(0, material);
+    var material = get_node("IguanaModel/10668_Iguana_v2").get_active_material(0).duplicate();
+    material.albedo_color = Color(255, 0, 0, 1);
+    get_node("IguanaModel/10668_Iguana_v2").set_surface_material(0, material);
 
 func hover_unfocus() -> void:
-    for index in model.get_surface_material_count():
-        var material = model.get_active_material(index).duplicate();
-        material.albedo_color = Color(0, 0, 0, 1);
-        model.set_surface_material(index, material);
+    var material = get_node("IguanaModel/10668_Iguana_v2").get_active_material(0).duplicate();
+    material.albedo_color = Color(0, 0, 0, 1);
+    get_node("IguanaModel/10668_Iguana_v2").set_surface_material(0, material);
 
 func interact() -> void:
     # TODO: Add in dialogic here
