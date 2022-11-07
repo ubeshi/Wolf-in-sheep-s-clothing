e00213028679fa1b8a2792c49cd8e9e5f933be90
Colin Tong <tongcolin@live.ca>
2022-11-06 23:31:59 -0500

    Disable arrows

--- src/scenes/Navigation.gd
+++ src/scenes/Navigation.gd
@@ -39,17 +39,17 @@ func set_camera_position(rotation):
     var camera = get_node("Camera")
     var camera_position = camera.get_camera_transform().origin
 
-    for i in camera_data:
-        if camera_data[i]["translation"] == camera_position:
-            print(i)
-            path = camera_path.find(i) + rotation
-            if (path > (camera_path.size() - 1)):
-                path = 0
-            print(camera_path[path])
-            position = camera_data[camera_path[path]]
-
-    camera.transform.origin = position["translation"]
-    camera.rotate_y(rotation*PI/2)
+#    for i in camera_data:
+#        if camera_data[i]["translation"] == camera_position:
+#            print(i)
+#            path = camera_path.find(i) + rotation
+#            if (path > (camera_path.size() - 1)):
+#                path = 0
+#            print(camera_path[path])
+#            position = camera_data[camera_path[path]]
+#
+#    camera.transform.origin = position["translation"]
+#    camera.rotate_y(rotation*PI/2)
 
 func _on_LeftButton_pressed():
     set_camera_position(-1)
