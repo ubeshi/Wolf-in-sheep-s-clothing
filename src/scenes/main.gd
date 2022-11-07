396f417abecf4ae70b1848b3e00ec842665793b7
Colin Tong <tongcolin@live.ca>
2022-11-06 23:06:56 -0500

    Duplicate debug scene and reimport

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -3,8 +3,8 @@ extends Spatial
 func _ready() -> void:
     # Add item menu
     add_scene("res://scenes/user-interface/item-menu.tscn");
-    # Add debug scene
-    add_scene("res://scenes/debug.tscn");
+    # Add ui scene
+    add_scene("res://scenes/ui.tscn");
 
 func add_scene(path: String) -> void:
     var scene = load(path);
