200e9e92c70a59c7a81ef47797174759a7a8c0f8
anson-li <ansonli.94@gmail.com>
2022-11-07 15:05:25 -0700

    Force original material

--- src/scenes/characters/wolf/Wolf.gd
+++ src/scenes/characters/wolf/Wolf.gd
@@ -1,14 +1,14 @@
 extends StaticBody
 
+onready var original_material = get_node("WolfModel/aleu001").get_active_material(0).duplicate();
+
 func hover_focus() -> void:
     var material = get_node("WolfModel/aleu001").get_active_material(0).duplicate();
     material.albedo_color = Color(255, 0, 0, 255);
     get_node("WolfModel/aleu001").set_surface_material(0, material);
 
 func hover_unfocus() -> void:
-    var material = get_node("WolfModel/aleu001").get_active_material(0).duplicate();
-    material.albedo_color = Color(255, 53, 53, 255);
-    get_node("WolfModel/aleu001").set_surface_material(0, material);
+    get_node("WolfModel/aleu001").set_surface_material(0, original_material);
 
 func interact() -> void:
     # TODO: Add in dialogic here
