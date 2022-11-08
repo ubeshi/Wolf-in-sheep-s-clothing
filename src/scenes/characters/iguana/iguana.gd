1eae6469a6b37dcc388411af99b18bcfd1ee2746
brydenli <73043856+brydenli@users.noreply.github.com>
2022-11-07 22:11:22 -0700

    Add dialog to onclick event for wolf and iguana

--- src/scenes/characters/iguana/iguana.gd
+++ src/scenes/characters/iguana/iguana.gd
@@ -1,8 +1,12 @@
 extends "res://scenes/characters/character.gd";
 
+onready var npc = "iguana";
+onready var dialog_index = 0;
+
 func _ready():
-    model = get_node("IguanaModel/10668_Iguana_v2");
-    original_material = get_node("IguanaModel/10668_Iguana_v2").get_surface_material(0).duplicate();
+	model = get_node("IguanaModel/10668_Iguana_v2");
+	original_material = get_node("IguanaModel/10668_Iguana_v2").get_surface_material(0).duplicate();
 
 func interact() -> void:
-    print('ribbit');
+	var dialog = Dialogic.start(npc + str(dialog_index))
+	get_parent().add_child(dialog)
