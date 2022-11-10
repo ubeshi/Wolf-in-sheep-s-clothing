99cadc08df87b1082ef352bbf16a6e704c77ceef
anson-li <ansonli.94@gmail.com>
2022-11-09 21:17:59 -0700

    Updated bunny horse

---
+++ src/scenes/characters/bunny/bunny.gd
@@ -0,0 +1,12 @@
+extends "res://scenes/characters/character.gd";
+
+onready var npc = "bunny";
+onready var dialog_index = 0;
+
+func _ready():
+    model = get_node("BunnyModel/bunnyMesh");
+    original_material = get_node("BunnyModel/bunnyMesh").get_surface_material(0).duplicate();
+
+func interact() -> void:
+    var dialog = Dialogic.start(npc + str(dialog_index));
+    get_parent().add_child(dialog);
