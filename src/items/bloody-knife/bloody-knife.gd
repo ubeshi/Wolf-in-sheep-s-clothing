edf964b3f7a5f4da24464ad7100d5304648ac735
adaj-1 <67992220+adaj-1@users.noreply.github.com>
2022-11-12 14:51:20 -0700

    fix spacing

--- src/items/bloody-knife/bloody-knife.gd
+++ src/items/bloody-knife/bloody-knife.gd
@@ -3,7 +3,7 @@ extends PlayerItem
 class_name BloodyKnife
 
 func _init():
-	self.icon_image_large = "res://assets/items/bloody-knife/bloody-knife-large.png";
-	self.icon_image_small = "res://assets/items/bloody-knife/bloody-knife-small.png";
-	self.label = "Bloody Knife";
-	self.description = "A kitchen knife with blood on it. The blood is still fresh, better not stain my suit.";
+    self.icon_image_large = "res://assets/items/bloody-knife/bloody-knife-large.png";
+    self.icon_image_small = "res://assets/items/bloody-knife/bloody-knife-small.png";
+    self.label = "Bloody Knife";
+    self.description = "A kitchen knife with blood on it. The blood is still fresh, better not stain my suit.";
