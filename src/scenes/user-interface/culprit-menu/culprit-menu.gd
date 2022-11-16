a47b118b1829796a1246e588e7b84322eb98a7d6
brydenli <73043856+brydenli@users.noreply.github.com>
2022-11-15 23:55:43 -0700

    Add support for all culprits in culprit menu

--- src/scenes/user-interface/culprit-menu/culprit-menu.gd
+++ src/scenes/user-interface/culprit-menu/culprit-menu.gd
@@ -1,10 +1,21 @@
 extends Menu
 
 var pig = Pig.new();
-
-var characters = [pig];
+var bunny = Bunny.new();
+var cat = Cat.new();
+var dog = Dog.new();
+var fox = Fox.new();
+var frog = Frog.new();
 
 var pig_hints = CulpritHints.pig_hints;
+var bunny_hints = CulpritHints.bunny_hints;
+var cat_hints = CulpritHints.cat_hints;
+var dog_hints = CulpritHints.dog_hints;
+var fox_hints = CulpritHints.fox_hints;
+var frog_hints = CulpritHints.frog_hints;
+
+var characters = [pig, bunny, cat, dog, fox, frog];
+var hints = [pig_hints, bunny_hints, cat_hints, dog_hints, fox_hints, frog_hints];
 
 var culprit_menu_body_node_path = "CanvasLayer/CulpritMenu/MarginContainer/ColorRect/MarginContainer/CulpritMenuBody";
 onready var culprit_menu_body_node = get_node(culprit_menu_body_node_path);
@@ -18,29 +29,55 @@ func _init() -> void:
     self.menu_path = "CanvasLayer/CulpritMenu";
 
 func handle_menu_opened() -> void:
-    set_selected_culprit(characters[0]);
+    var culprit_list_node = culprit_menu_body_node.get_node("CulpritList");
+    var buttons = get_culprit_list_buttons(characters);
+    for button in buttons:
+        culprit_list_node.add_child(button);
 
-func get_culprit_list(culprits: Array) -> Array:
+func handle_menu_closed() -> void:
+    var culprit_list_node = culprit_menu_body_node.get_node("CulpritList");
+    for culprit in culprit_list_node.get_children():
+        culprit_list_node.remove_child(culprit);
+        culprit.queue_free();
+
+func get_culprit_list_buttons(culprits: Array) -> Array:
     var buttons = [];
+    var i = 0;
     for culprit in culprits:
         var button = TextureButton.new();
         var texture = load(culprit.image);
         button.texture_normal = texture;
-        button.connect("pressed", self, "set_selected_culprit", [culprit]);
+        button.connect("pressed", self, "set_selected_culprit", [culprit, i]);
         buttons.append(button);
+        i += 1;
     return buttons;
 
-func set_selected_culprit(culprit: Culprit) -> void:
+func set_selected_culprit(culprit: Culprit, index: int) -> void:
     var selectedCulpritNode = culprit_menu_body_node.get_node("SelectedCulprit");
     var selectedItemImageNode = selectedCulpritNode.get_node("CulpritImage");
     var selectedItemLabelNode = selectedCulpritNode.get_node("CulpritText/CulpritLabel");
     var selectedItemDescriptionNode = selectedCulpritNode.get_node("CulpritText/CulpritDescription");
     var arr = [1, 2, 3];
+    var culprit_hints;
+    match culprit:
+        pig:
+            culprit_hints = pig_hints;
+        bunny:
+            culprit_hints = bunny_hints;
+        cat:
+            culprit_hints = cat_hints;
+        dog:
+            culprit_hints = dog_hints;
+        fox:
+            culprit_hints = fox_hints;
+        frog:
+            culprit_hints = frog_hints;
+
     for i in arr:
         var node_path = "CulpritText/CulpritHint" + str(i);
         var selectedHintNode = selectedCulpritNode.get_node(node_path);
-        if pig_hints.size() >= i:
-            selectedHintNode.text = pig_hints[i - 1];
+        if culprit_hints.size() >= i:
+            selectedHintNode.text = culprit_hints[i - 1];
         else:
             selectedHintNode.text = "hint not available";
 
