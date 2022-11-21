043ad9b59c1ff7d3d8a5444702b17e6cd1058d04
Anson Li <hello@ansonli.ca>
2022-11-21 08:54:56 -0700

    Updated bunny model (#81)

--- src/scenes/characters/bunny/bunny.gd
+++ src/scenes/characters/bunny/bunny.gd
@@ -4,8 +4,9 @@ func _init():
     self.npc = "bunny";
 
 func _ready():
-    model = get_node("BunnyModel/bunnyMesh");
-    original_material = get_node("BunnyModel/bunnyMesh").get_surface_material(0).duplicate();
+    model = get_node("BunnyModel/1");
+    original_material = get_node("BunnyModel/1").get_surface_material(4).duplicate();
+    surface_material = 4;
 
 func interact() -> void:
     var dialog = Dialogic.start(npc + str(dialog_index));
