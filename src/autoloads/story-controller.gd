65ae7b32386cd2d8da6f1762e710f42540cde8d8
brydenli <73043856+brydenli@users.noreply.github.com>
2022-11-21 20:05:48 -0700

    Add semicolons

--- src/autoloads/story-controller.gd
+++ src/autoloads/story-controller.gd
@@ -1,16 +1,16 @@
 extends Node
 
-var current_scene = null
+var current_scene = null;
 
 func _ready():
-    var root = get_tree().root
-    current_scene = root.get_child(root.get_child_count() - 1)
+    var root = get_tree().root;
+    current_scene = root.get_child(root.get_child_count() - 1);
 
 func go_to_scene(path):
-    call_deferred("_deferred_goto_scene", path)
+    call_deferred("_deferred_goto_scene", path);
 
 func deferred_go_to_scene(path):
-    current_scene.free()
-    var s = ResourceLoader.load(path)
-    current_scene = s.instance()
-    get_tree().root.add_child(current_scene)
+    current_scene.free();
+    var s = ResourceLoader.load(path);
+    current_scene = s.instance();
+    get_tree().root.add_child(current_scene);
