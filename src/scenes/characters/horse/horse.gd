40e2d2a1dc6b021ad7640004a7c9b9f584d70b08
adaj-1 <67992220+adaj-1@users.noreply.github.com>
2022-11-22 20:48:28 -0700

    resolved conflicts and fixed bug where you were unable to talk to horse as it had no quest

--- src/scenes/characters/horse/horse.gd
+++ src/scenes/characters/horse/horse.gd
@@ -2,6 +2,11 @@ extends "res://scenes/characters/character.gd";
 
 func _init():
     self.npc = "horse";
+    self.fetch_quest = {
+        start_signal = '',
+        end_signal = '',
+        quest = EmptyQuest.new()
+       };
 
 func _ready():
     model = get_node("HorseModel/16267_American_Paint_Horse_Nuetral_new");
