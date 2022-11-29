908940f177173a30c9cb4bd82f717e7e2ac197ae
Bryden Li <brydenli.97@gmail.com>
2022-11-28 18:56:47 -0700

    Update bunny description

--- src/culprits/bunny/bunny.gd
+++ src/culprits/bunny/bunny.gd
@@ -6,7 +6,7 @@ func _init():
     self.image_small = "res://assets/culprits/bunny/bunny.png";
     self.image_large = "res://assets/culprits/bunny/bunny_large.png";
     self.label = "Bunny";
-    self.description = "Otherwise known as Boingo. Likes to make carrot cakes and puddings.";
+    self.description = "Otherwise known as Boingo. Likes to make carrot cakes and pudding.";
     self.hints = [
         "Says it was in the kitchen during the murder.",
         "Seems to have lost its bag, where it stored the sharp cooking knife...?",
