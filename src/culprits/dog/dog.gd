bc64f4321f54dd7b74b44e3ed5e7c2cd61a8e042
Bryden Li <brydenli.97@gmail.com>
2022-11-28 18:43:04 -0700

    Update culprit descriptions and hints

--- src/culprits/dog/dog.gd
+++ src/culprits/dog/dog.gd
@@ -6,5 +6,9 @@ func _init():
     self.image_small = "res://assets/culprits/dog/dog.png";
     self.image_large = "res://assets/culprits/dog/dog_large.png";
     self.label = "Dog";
-    self.description = "Is a good guy.";
-    self.hints = ["Dog hint 1", "Dog hint 2", "Dog hint 3"];
+    self.description = "Likes to dig holes and eat from bowls.";
+    self.hints = [
+        "Hung out with Fox in the living room, went to the garden, and then stayed in the basement with Cat and Frog.",
+        "Saw Bunny leaving the kitchen to the living room. Looked like Bunny was carrying a heavy bag.",
+        "Found a bloody knife in the garden. Did the killer leave through the window?",
+    ];
